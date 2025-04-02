2ea85f90ada8adb8d64046bf82dce6ff69ab419b
LuisNorberto1998 <luis_norberto1998@hotmail.com>
2025-04-02 15:32:42 -0600

    Fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './home/home-page/home-page.component';
-import { AboutPageComponent } from './about/about-page/about-page.component';
 
 const routes: Routes = [
-  { path: '', component: HomePageComponent },  // Ruta para la página de inicio
-  { path: 'about', component: AboutPageComponent },  // Ruta para la página "About"
-  { path: '**', redirectTo: '' }  // Ruta por defecto (404, redirige a Home)
+  { path: '', component: HomePageComponent }, 
+  { path: '**', redirectTo: '' } 
 ];
 
 @NgModule({
